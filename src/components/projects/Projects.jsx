9aa103636d1d6e7ca97a807de21b4fccf81ff3ee
Sahil Rai <sahil.rai@gl09641m.gamesys.corp>
2023-11-21 22:11:28 +0000

    feat: integrated basic github api call to project compononet

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import GitHubAPI from './GitHubAPI';
 
 function Projects() {
   return (
     <div>
       <section className='section' id='projects'>
         Projects
+        <div>
+          <GitHubAPI />
+        </div>
       </section>
     </div>
   );
